484ac3aac77f75206a77e70835a7ea983a7a7aa8
Sebastian Wilzbach <seb@wilzba.ch>
2017-02-12 14:56:56 +0100

    Rename DUB package to hb-ddb

--- test/source.d
+++ test/source.d
@@ -1,44 +1,58 @@
-void runTest()
+void runTest() @safe
 {
-    import ddb.postgres : PostgresDB, PGCommand;
+    import ddb.pg : PostgresDB, PGCommand;
     import std.process : environment;
     import std.stdio;
 
-	auto pdb = new PostgresDB([
-		"host" : environment.get("DB_HOST", "localhost"),
-		"database" : environment["DB_NAME"],
-		"user" : environment["DB_USER"],
-		"password" : environment["DB_PASSWORD"]
-	]);
-	auto conn = pdb.lockConnection();
+    auto pdb = new PostgresDB([
+        "host" : environment.get("DB_HOST", "localhost"),
+        "database" : environment["DB_NAME"],
+        "user" : environment["DB_USER"],
+        "password" : environment["DB_PASSWORD"]
+    ]);
+    auto conn = pdb.lockConnection();
+    scope(exit) conn.close;
 
-	auto cmd = new PGCommand(conn, "SELECT typname, typlen FROM pg_type");
-	auto result = cmd.executeQuery;
-	try
-	{
-		foreach (row; result)
-		{
-			writeln(row["typname"], ", ", row[1]);
-		}
-	}
-	finally
-	{
-		result.close;
-	}
+    // prepared statement
+    {
+        auto cmd = new PGCommand(conn, `SELECT * from "LoanRequests" Limit 1`);
+        auto result = cmd.executeQuery();
+        scope(exit) () @trusted { result.destroy; }();
+        foreach (row; result)
+            writeln(row);
+    }
+
+    // query
+    with (conn.transaction) {
+        auto result2 = conn.query(`SELECT * from "LoanRequests" Limit 1`);
+        scope(exit) () @trusted { result2.destroy; }();
+        foreach (row; result2)
+            writeln(row);
+    }
+
+    conn.transaction({
+        auto result2 = conn.query(`SELECT * from "LoanRequests" Limit 1`);
+        scope(exit) () @trusted { result2.destroy; }();
+        foreach (row; result2)
+            writeln(row);
+    });
+
+    conn.query("LISTEN foo");
+    conn.execute("NOTIFY foo, 'bar'");
 }
 
 int main()
 {
     import vibe.core.core, vibe.core.log;
-	int ret = 0;
-	runTask({
-		try runTest();
-		catch (Throwable th) {
-			logError("Test failed: %s", th.msg);
-			logDiagnostic("Full error: %s", th);
-			ret = 1;
-		} finally exitEventLoop(true);
-	});
-	runEventLoop();
-	return ret;
+    int ret = 0;
+    runTask({
+        try runTest();
+        catch (Throwable th) {
+            logError("Test failed: %s", th.msg);
+            logDiagnostic("Full error: %s", th);
+            ret = 1;
+        } finally exitEventLoop(true);
+    });
+    runEventLoop();
+    return ret;
 }
